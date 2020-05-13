ef00b84c9252d8de989adbf0b09f7a44d82f0a74
elias <elias@DESKTOP-8UEOB40>
2020-05-13 17:30:36 +0300

    initial commit

---
+++ Crowdfund/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crowdfund
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Team6");
+        }
+    }
+}
