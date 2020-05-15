3f864c9191de25b7f8e3948b8486fce5cf6f2559
elias <elias@DESKTOP-8UEOB40>
2020-05-15 15:47:46 +0300

    new commit

--- CrowdfundPrivate/Program.cs
+++ CrowdfundPrivate/Program.cs
@@ -7,34 +7,7 @@ namespace Crowdfund
     {
         static void Main(string[] args)
         {
-            var prjctcret = new User()
-            {
-
-                UserName = "elias",
-
-            };
-            var prjct = new Project() { 
-                Title = "test"
-            };
-
-            var rwrd = new Reward() {
-                Title = " reward test ",
-                RewardId = 1,
-                
-            
-            };
-
-           
-           
-
-            prjctcret.Projects.Add(prjct);
-            prjct.AvailableRewards.Add(rwrd);
-
-            var stst = new StatusUpdate();
-            
-            
-
-
+            Console.WriteLine("New Commit");
         }
     }
 }
