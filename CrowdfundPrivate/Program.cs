d961445c3b0edd47f9a487b53cb4f2a79bbf79f5
Spyros <Spyros@DESKTOP-EHCEVFL>
2020-05-15 15:46:37 +0300

    commit-spyros

--- CrowdfundPrivate/Program.cs
+++ CrowdfundPrivate/Program.cs
@@ -7,31 +7,8 @@ namespace Crowdfund
     {
         static void Main(string[] args)
         {
-            var prjctcret = new User()
-            {
 
-                UserName = "elias",
-
-            };
-            var prjct = new Project() { 
-                Title = "test"
-            };
-
-            var rwrd = new Reward() {
-                Title = " reward test ",
-                RewardId = 1,
-                
-            
-            };
-
-           
-           
-
-            prjctcret.Projects.Add(prjct);
-            prjct.AvailableRewards.Add(rwrd);
-
-            var stst = new StatusUpdate();
-            
+            Console.WriteLine("Spyros");
             
 
 
